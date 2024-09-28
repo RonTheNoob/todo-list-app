f5dcb4e18042cc1d825116ba3214037dfbe6c587
Ronnie Borromeo Jr. <162797260+RonTheNoob@users.noreply.github.com>
2024-09-28 10:49:56 +0800

    My initial server

---
+++ server/index.js
@@ -0,0 +1,13 @@
+// express package
+import express from "express";
+const app = express();
+
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello Todo App!!!");
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
